f9e1eb367be32aa5d756a3a42653d31f8e74a33a
Mickael DP <m.dallepasqualine@gmail.com>
2023-04-13 12:57:44 +0200

    Finish Design CardGroup

--- src/components/GroupesList/index.jsx
+++ src/components/GroupesList/index.jsx
@@ -93,6 +93,8 @@ function GroupesList({ access = "all" }) {
       container
       spacing={{ xs: 2, md: 3 }}
       columns={{ xs: 4, sm: 8, md: 12 }}
+      direction="row"
+      alignItems="stretch"
     >
       {groupes.map((groupe, index) => (
         <Grid item xs={2} sm={4} md={4} key={index}>
