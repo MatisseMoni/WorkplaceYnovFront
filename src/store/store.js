c681ddadcd6332541e7bc279056ab1d0638891d6
MatisseMoni <matissemonipro@gmail.com>
2023-04-13 15:59:40 +0200

    loading

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./reducers/user";
 import authSlice from "./reducers/auth";
 import groupeSlice from "./reducers/groupe";
+import loadingSlice from "./reducers/loading";
 
 export default configureStore({
     reducer: {
         user: userSlice,
         auth: authSlice,
-        groupe: groupeSlice
+        groupe: groupeSlice,
+        loading: loadingSlice,
     }
 });
