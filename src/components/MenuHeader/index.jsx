9b8f3880d0c4df39a50cc2117fea340ab8b64832
Mickael DP <m.dallepasqualine@gmail.com>
2023-04-13 10:51:47 +0200

    fix design header

--- src/components/MenuHeader/index.jsx
+++ src/components/MenuHeader/index.jsx
@@ -18,177 +18,222 @@ import { logout } from '../../store/reducers/auth';
 import { useNavigate } from 'react-router-dom';
 
 function ResponsiveAppBar() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
 
-    const handleLogout = () => {
-        dispatch(logout());
-        localStorage.removeItem('token');
-        navigate('/');
-    };
+	const handleLogout = () => {
+		dispatch(logout());
+		localStorage.removeItem('token');
+		navigate('/');
+	};
 
-    const [logged, setLogged] = React.useState(false);
-    const [user, setUser] = React.useState({});
-    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-    const userLog = useSelector(state => state.auth.user);
+	const [logged, setLogged] = React.useState(false);
+	const [user, setUser] = React.useState({});
+	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+	const userLog = useSelector((state) => state.auth.user);
 
-    React.useEffect(() => {
-        setLogged(isLoggedIn);
-        setUser(userLog);
-    }, [isLoggedIn, userLog]);
+	React.useEffect(() => {
+		setLogged(isLoggedIn);
+		setUser(userLog);
+	}, [isLoggedIn, userLog]);
 
-    const links = [
-        { name: "User List", path: "/userList" },
-        { name: "Connexion", path: "/connexion" },
-        { name: "Inscription", path: "/inscription" },
-    ]
-    const settings = [
-        { name: 'Compte', path: '/compte' },
-        { name: 'Deconnexion', onClick: () => handleLogout()}]
+	const links = [];
 
-    return (
-        <AppBar position="static" sx={{ marginBottom: 5}}>
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        WORKSPACE YNOV
-                    </Typography>
+	const linksisnotlogged = [
+		{ name: 'Connexion', path: '/connexion' },
+		{ name: 'Inscription', path: '/inscription' },
+	];
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            {links.map((link, index) => (
-                                <MenuItem key={index} onClick={handleCloseNavMenu}>
-                                    <Link to={link.path}><Typography textAlign="center">{link.name}</Typography></Link>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                    <Link to="/">
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="h5"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        LOGO
-                    </Typography></Link>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {links.map((link, index) => (
-                            <Link to={link.path}><Button
-                                key={index}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {link.name}
-                            </Button></Link>
-                        ))}
-                    </Box>
-                    {logged ? (
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt={user.nickname} src="/static/images/avatar/2.jpg" />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting, index) => (
-                                    <MenuItem key={index} onClick={handleCloseUserMenu}>
-                                        {setting.path ?
-                                            (<Link to={setting.path}><Typography textAlign="center">{setting.name}</Typography></Link>) :
-                                            (<Typography textAlign="center" onClick={setting.onClick}>{setting.name}</Typography>)}
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>) : null}
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+	const settings = [
+		{ name: 'Compte', path: '/compte' },
+		{ name: 'Deconnexion', onClick: () => handleLogout() },
+	];
+
+	return (
+		<AppBar
+			position='static'
+			sx={{ marginBottom: 5, background: 'white' }}>
+			<Container maxWidth='xl' >
+				<Toolbar disableGutters>
+					<Typography
+						variant='h5'
+						noWrap
+						component='a'
+						href='/'
+						sx={{
+							mr: 2,
+							display: { xs: 'none', md: 'flex' },
+							fontWeight: 700,
+							letterSpacing: '.3rem',
+							color: 'black',
+							textDecoration: 'none',
+						}}>
+						Ynov Workplace
+					</Typography>
+
+					<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }}}>
+						<IconButton
+							size='large'
+							aria-label='account of current user'
+							aria-controls='menu-appbar'
+							aria-haspopup='true'
+							onClick={handleOpenNavMenu}
+							color='black'>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id='menu-appbar'
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: 'bottom',
+								horizontal: 'left',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: 'block', md: 'none' },
+							}}>
+							{logged
+								? links.map((link, index) => (
+										<Link to={link.path} style={{ textDecoration: 'none' }}>
+											<Button
+												key={index}
+												onClick={handleCloseNavMenu}
+												sx={{ my: 2, color: 'black', display: 'block' }}
+                                                >
+												{link.name}
+											</Button>
+										</Link>
+								  ))
+								: linksisnotlogged.map((linkisnotlogged, index) => (
+										<Link to={linkisnotlogged.path} style={{ textDecoration: 'none' }}>
+											<Button
+												key={index}
+												onClick={handleCloseNavMenu}
+												sx={{ my: 2, color: 'black', display: 'block' }}>
+												{linkisnotlogged.name}
+											</Button>
+										</Link>
+								  ))}
+						</Menu>
+					</Box>
+					<Link to='/' style={{ textDecoration: 'none' }}>
+						<Typography
+							variant='h5'
+							noWrap
+							component='h5'
+							href='/'
+							sx={{
+								mr: 2,
+								display: { xs: 'flex', md: 'none' },
+								flexGrow: 1,
+								fontWeight: 700,
+								letterSpacing: '.3rem',
+								color: 'black',
+								textDecoration: 'none',
+							}}>
+							Ynov Workplace
+						</Typography>
+					</Link>
+					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent:'end',}}>
+						{logged
+							? links.map((link, index) => (
+									<Link to={link.path} style={{ textDecoration: 'none' }}>
+										<Button
+											key={index}
+											onClick={handleCloseNavMenu}
+											sx={{ my: 2, color: 'black', display: 'block' }}
+                                            underline="none">
+											{link.name}
+										</Button>
+									</Link>
+							  ))
+							: linksisnotlogged.map((linkisnotlogged, index) => (
+									<Link to={linkisnotlogged.path} style={{ textDecoration: 'none' }}>
+										<Button
+											key={index}
+											onClick={handleCloseNavMenu}
+											sx={{ my: 2, color: 'black', display: 'block' }}>
+											{linkisnotlogged.name}
+										</Button>
+									</Link>
+							  ))}
+					</Box>
+					{logged ? (
+						<Box sx={{ flexGrow: 0 }}>
+							<Tooltip title='Open settings'>
+								<IconButton
+									onClick={handleOpenUserMenu}
+									sx={{ p: 0 }}>
+									<Avatar
+										alt={user.nickname}
+										src='/static/images/avatar/2.jpg'
+									/>
+								</IconButton>
+							</Tooltip>
+							<Menu
+								sx={{ mt: '45px' }}
+								id='menu-appbar'
+								anchorEl={anchorElUser}
+								anchorOrigin={{
+									vertical: 'top',
+									horizontal: 'right',
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: 'top',
+									horizontal: 'right',
+								}}
+								open={Boolean(anchorElUser)}
+								onClose={handleCloseUserMenu}>
+								{settings.map((setting, index) => (
+									<MenuItem
+										key={index}
+										onClick={handleCloseUserMenu}>
+										{setting.path ? (
+											<Link to={setting.path}>
+												<Typography textAlign='center'>
+													{setting.name}
+												</Typography>
+											</Link>
+										) : (
+											<Typography
+												textAlign='center'
+												onClick={setting.onClick}>
+												{setting.name}
+											</Typography>
+										)}
+									</MenuItem>
+								))}
+							</Menu>
+						</Box>
+					) : null}
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
 export default ResponsiveAppBar;
