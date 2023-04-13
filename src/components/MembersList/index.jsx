acf4852834eab1b4f1c92eb0eb1f48960ce22997
Mickael DP <m.dallepasqualine@gmail.com>
2023-04-13 16:58:51 +0200

    design page group in progress

--- src/components/MembersList/index.jsx
+++ src/components/MembersList/index.jsx
@@ -1,41 +1,50 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from '@mui/material/Card';
+import Container from '@mui/material/Container';
 
-function MembersList ({ groupId, owner }) {
-    const [members, setMembers] = useState([])
-    const [error, setError] = useState(null)
-    const url = `${process.env.REACT_APP_YOUR_API_URL}/api/groups`;
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await axios.get(`${url}/${groupId}/members`)
-                const membersTmp = response.data["hydra:member"]
-                membersTmp.forEach(member => {
-                    member.isOwner = member["@id"] === owner
-                })
-                console.log(membersTmp)
-                setMembers(membersTmp)
-            }
-            catch (error) {
-                console.error(error)
-                setError(error)
-            }
+function MembersList({ groupId, owner }) {
+	const [members, setMembers] = useState([]);
+	const [error, setError] = useState(null);
+	const url = `${process.env.REACT_APP_YOUR_API_URL}/api/groups`;
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await axios.get(`${url}/${groupId}/members`);
+				const membersTmp = response.data['hydra:member'];
+				membersTmp.forEach((member) => {
+					member.isOwner = member['@id'] === owner;
+				});
+				console.log(membersTmp);
+				setMembers(membersTmp);
+			} catch (error) {
+				console.error(error);
+				setError(error);
+			}
+		})();
+	}, []);
 
-        })()
-    }, [])
-
-  return (
-    <>
-    <h3>Membres</h3>
-    {members.length > 0 ? (<ul>
-      {members.map(member => (
-        <li key={member.id}>
-            <h3>{member.nickname} {member.isOwner ? (<span>(admin)</span>) : null}</h3>
-        </li>
-      ))}
-    </ul>) : (<p>Aucun membre</p>)}
-    </>
-  )
+	return (
+		<Card sx={{ width: '300px' }}>
+			<Container>
+				<h3>Membres</h3>
+				{members.length > 0 ? (
+					<ul>
+						{members.map((member) => (
+							<li key={member.id}>
+								<h3>
+									{member.nickname}{' '}
+									{member.isOwner ? <span>(admin)</span> : null}
+								</h3>
+							</li>
+						))}
+					</ul>
+				) : (
+					<p>Aucun membre</p>
+				)}
+			</Container>
+		</Card>
+	);
 }
 
-export default MembersList
+export default MembersList;
--- src/components/ThreadsList/index.jsx
+++ src/components/ThreadsList/index.jsx
@@ -1,45 +1,61 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
 
 function ThreadsList({ groupeId }) {
-  const [threads, setThreads] = useState([]);
-  const url = `${process.env.REACT_APP_YOUR_API_URL}/api/search`;
+	const [threads, setThreads] = useState([]);
+	const url = `${process.env.REACT_APP_YOUR_API_URL}/api/search`;
 
-  useEffect(() => {
-    (async () => {
-      const token = localStorage.getItem("token");
-      const response = await axios.get(url, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      const threadsTmp = response.data["hydra:member"].filter((thread) => {
-        return thread.relatedGroup === `/api/groups/${groupeId}`;
-      });
-      setThreads(threadsTmp);
-    })();
-  }, [groupeId]);
+	useEffect(() => {
+		(async () => {
+			const token = localStorage.getItem('token');
+			const response = await axios.get(url, {
+				headers: { Authorization: `Bearer ${token}` },
+			});
+			const threadsTmp = response.data['hydra:member'].filter((thread) => {
+				return thread.relatedGroup === `/api/groups/${groupeId}`;
+			});
+			setThreads(threadsTmp);
+		})();
+	}, [groupeId]);
 
-  return (
-    <>
-      <h3>Threads</h3>
-      <Link to={`/groupes/${groupeId}/createThread`}>Ajouter un Thread</Link>
-      <ul>
-        {threads.map((thread) => {
-          const id = thread["@id"].split("/")[3];
+	return (
+		<Card sx={{ width: '300px' }}>
+			<Container>
+				<Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+					<Typography
+						variant='h6'
+						component='h1'
+						textAlign='center'>
+						Threads
+					</Typography>
+					<Link to={`/groupes/${groupeId}/createThread`}>
+						Ajouter
+					</Link>
+				</Box>
 
-          return (
-            <li>
-              <h3>{thread.title}</h3>
-              <p>{thread.slug}</p>
-              <Link to={`/groupes/${groupeId}/threads/${id}`}>
-                Voir le thread
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </>
-  );
+				<ul>
+					{threads.map((thread) => {
+						const id = thread['@id'].split('/')[3];
+
+						return (
+							<li>
+								<h3>{thread.title}</h3>
+								<p>{thread.slug}</p>
+								<Link to={`/groupes/${groupeId}/threads/${id}`}>
+									Voir le thread
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</Container>
+		</Card>
+	);
 }
 
 export default ThreadsList;
