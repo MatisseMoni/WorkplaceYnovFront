e5480bbd59f879a4026047cb8e0096db4e3a3cbc
Mickael DP <m.dallepasqualine@gmail.com>
2023-05-03 23:18:59 +0200

    Design Thread & Tchat

--- src/components/Tchat/index.jsx
+++ src/components/Tchat/index.jsx
@@ -1,90 +1,134 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Loader from "../Loader";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Loader from '../Loader';
+import { useSelector } from 'react-redux';
+import {
+	Box,
+	Container,
+	Grid,
+	Typography,
+	Button,
+	List,
+	ListItem,
+	ListItemText,
+	TextField,
+} from '@mui/material';
 
 function Tchat({ thread }) {
-    const id = thread['@id'].split('/')[3];
-  const url = `${process.env.REACT_APP_YOUR_API_URL}/api/threads/${id}/messages`;
-  const urlSend = `${process.env.REACT_APP_YOUR_API_URL}/api/messages`;
-  const currentUser = useSelector((state) => state.auth.user);
+	const id = thread['@id'].split('/')[3];
+	const url = `${process.env.REACT_APP_YOUR_API_URL}/api/threads/${id}/messages`;
+	const urlSend = `${process.env.REACT_APP_YOUR_API_URL}/api/messages`;
+	const currentUser = useSelector((state) => state.auth.user);
 
-  const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(true);
+	const [messages, setMessages] = useState([]);
+	const [message, setMessage] = useState('');
+	const [loading, setLoading] = useState(true);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    (async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const response = await axios.post(
-            urlSend,
-          {
-            content: message,
-            thread: thread["@id"],
-          },
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          }
-        );
-        setMessages([...messages, response.data]);
-        setMessage("");
-      } catch (error) {
-        console.error(error);
-      }
-    })();
-  }
+	function handleSubmit(event) {
+		event.preventDefault();
+		(async () => {
+			try {
+				const token = localStorage.getItem('token');
+				const response = await axios.post(
+					urlSend,
+					{
+						content: message,
+						thread: thread['@id'],
+					},
+					{
+						headers: { Authorization: `Bearer ${token}` },
+					}
+				);
+				setMessages([...messages, response.data]);
+				setMessage('');
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const response = await axios.get(url, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-        setMessages(response.data["hydra:member"]);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
-  }, [url]);
+	useEffect(() => {
+		(async () => {
+			try {
+				const token = localStorage.getItem('token');
+				const response = await axios.get(url, {
+					headers: { Authorization: `Bearer ${token}` },
+				});
+				setMessages(response.data['hydra:member']);
+				setLoading(false);
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}, [url]);
 
-  if (loading) {
-    return <Loader />;
-  }
+	if (loading) {
+		return <Loader />;
+	}
 
-  return (
-    <div>
-      <h1>Messages</h1>
-      {messages.length === 0 && !loading ? (
-        <div>Il n'y a pas de messages</div>
-      ) : (
-        <ul>
-          {messages.map((message) => (
-            <li key={message.id}>
-              <p>{message.content}</p>
-              {`/api/users/${currentUser.id}` === message.owner ? 
-              (<h2>Me</h2>) : (<h2>{message.owner}</h2>)}
-            </li>
-          ))}
-        </ul>
-      )}
-      <form>
-        <input
-          type="text"
-          value={message}
-          onChange={(event) => {
-            setMessage(event.target.value);
-          }}
-        />
-        <button type="submit" onClick={(e) => handleSubmit(e)}>
-          Envoyer
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<Container>
+			<Typography
+				variant='h3'
+				align='center'>
+				Messages
+			</Typography>
+			{messages.length === 0 && !loading ? (
+				<Typography
+					variant='subtitle1'
+					align='center'>
+					Il n'y a pas de messages
+				</Typography>
+			) : (
+				<List>
+					{messages.map((message) => (
+						<ListItem key={message.id}>
+							<ListItemText primary={message.content} />
+							<ListItemText
+								secondary={
+									`/api/users/${currentUser.id}` === message.owner
+										? 'Me'
+										: message.owner
+								}
+								align='right'
+							/>
+						</ListItem>
+					))}
+				</List>
+			)}
+			<Box mt={3}>
+				<form onSubmit={(e) => handleSubmit(e)}>
+					<Grid
+						container
+						spacing={2}
+						alignItems='center'>
+						<Grid
+							item
+							xs={10}>
+							<TextField
+								fullWidth
+								variant='outlined'
+								placeholder='Entrez votre message...'
+								value={message}
+								onChange={(event) => setMessage(event.target.value)}
+							/>
+						</Grid>
+						<Grid
+							item
+							xs={2}>
+							<Button
+								fullWidth
+								type='submit'
+								variant='contained'
+								color='primary'>
+								Envoyer
+							</Button>
+						</Grid>
+					</Grid>
+				</form>
+			</Box>
+		</Container>
+	);
 }
 
 export default Tchat;
--- src/pages/Thread.jsx
+++ src/pages/Thread.jsx
@@ -1,41 +1,63 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
-import Loader from "../components/Loader";
-import Tchat from "../components/Tchat";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Loader from '../components/Loader';
+import Tchat from '../components/Tchat';
+import { Box, Container, Card, CardContent, Divider, Typography } from '@mui/material';
 
 function Thread() {
-    let { idGroupe ,idThread } = useParams();
-    const url = `${process.env.REACT_APP_YOUR_API_URL}/api/threads/`;
-    const [thread, setThread] = useState(null);
+	let { idGroupe, idThread } = useParams();
+	const url = `${process.env.REACT_APP_YOUR_API_URL}/api/threads/`;
+	const [thread, setThread] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const token = localStorage.getItem("token");
-                const response = await axios.get(`${url}${idThread}`, {
-                    headers: { Authorization: `Bearer ${token}` },
-                });
-                setThread(response.data);
-                console.log(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        })();
-    }, [idGroupe, idThread]);
+	useEffect(() => {
+		(async () => {
+			try {
+				const token = localStorage.getItem('token');
+				const response = await axios.get(`${url}${idThread}`, {
+					headers: { Authorization: `Bearer ${token}` },
+				});
+				setThread(response.data);
+				console.log(response.data);
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}, [idGroupe, idThread]);
 
-    if (!thread) {
-        return <Loader />;
-    }
+	if (!thread) {
+		return <Loader />;
+	}
 
-    return (
-        <div>
-            <h1>{thread.title}</h1>
-            <p>{thread.content}</p>
-            <p>{thread.owner}</p>
-            <Tchat thread={thread} />
-        </div>
-    );
-    }
+	return (
+		<Container>
+			<Card>
+				<CardContent>
+					<Typography
+						variant='h5'
+						component='h2'
+						align='center'>
+						{thread.title}
+					</Typography>
+					<Box
+						sx={{
+							marginTop: '20px',
+							marginBottom: '20px',
+							textAlign: 'center',
+						}}>
+						<Typography variant='subtitle1'>Créé par {thread.owner}</Typography>
+						<Typography variant='subtitle1'>
+							Objet : {thread.content}
+						</Typography>
+					</Box>
+					<Divider />
+					<Box sx={{ marginTop: '20px', marginBottom: '20px' }}>
+						<Tchat thread={thread} />
+					</Box>
+				</CardContent>
+			</Card>
+		</Container>
+	);
+}
 
 export default Thread;
