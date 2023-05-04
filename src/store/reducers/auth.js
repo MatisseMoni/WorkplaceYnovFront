9b1edf9b3e1055658460d92aeeb2138d09eb8cde
MatisseMoni <matissemonipro@gmail.com>
2023-05-04 14:15:26 +0200

    logout socket

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -10,12 +10,15 @@ const authSlice = createSlice({
         login: (state, action) => {
             const ENDPOINT = "http://127.0.0.1:4001";
             const socket = socketIOClient(ENDPOINT);
-            socket.emit("user login", {username: action.payload.nickname});
-
+            socket.emit("user login", {user: action.payload});
             state.isLoggedIn = true;
             state.user = action.payload;
         },
         logout: (state) => {
+            const ENDPOINT = "http://127.0.0.1:4001";
+            const socket = socketIOClient(ENDPOINT);
+            socket.emit("user logout", {userId: state.user.id});
+
             state.isLoggedIn = false;
             state.user = null;
         },
